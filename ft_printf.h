7fd4987a6a685d5f168eed926288248068ac418e
Alyona Golz <emax@coex.tech>
2023-02-01 19:00:46 +0400

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -32,6 +32,24 @@ typedef struct s_form
 	char	format;
 }						t_form;
 
+t_format	ft_newformat(void)
+{
+	t_format	newformat;
+
+	newformat.minus = 0;
+	newformat.plus = 0;
+	newformat.width = 0;
+	newformat.precision = 0;
+	newformat.specifier = 0;
+	newformat.zero = 0;
+	newformat.dot = 0;
+	newformat.space = 0;
+	newformat.sharp = 0;
+	newformat.neg_prec = 0;
+	return (newformat);
+}
+
+
 # define SPEC "csdiupxX%"
 
 #endif 
